7316b88649cc50733e3af70e28494d0adee4fa86
Ivan Vergés <ivan@pokecode.net>
2024-06-04 11:26:35 +0200

    remove port from letter_opener

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,6 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.default_url_options = { port: 3000 }
 
   config.action_mailer.perform_caching = false
 
