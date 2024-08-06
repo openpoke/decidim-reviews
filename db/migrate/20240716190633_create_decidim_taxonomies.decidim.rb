0a9fba29252fad0ad210a489b95565344133395a
Ivan Vergés <ivan@pokecode.net>
2024-08-06 21:36:58 +0200

    add filter controllers

--- db/migrate/20240716190633_create_decidim_taxonomies.decidim.rb
+++ db/migrate/20240716190633_create_decidim_taxonomies.decidim.rb
@@ -1,37 +1,23 @@
 # frozen_string_literal: true
 
-# This migration comes from decidim (originally 20240704115429)
 class CreateDecidimTaxonomies < ActiveRecord::Migration[7.0]
   def change
     create_table :decidim_taxonomies do |t|
       t.jsonb :name, null: false, default: {}
-      t.integer :decidim_organization_id, null: false
-      t.integer :parent_id
+      t.references :decidim_organization, null: false, index: true
+      t.references :parent, index: true
       t.integer :weight
+      t.integer :children_count, null: false, default: 0
+      t.integer :taxonomizations_count, null: false, default: 0
       t.timestamps
     end
-
-    add_index :decidim_taxonomies, :decidim_organization_id
-    add_index :decidim_taxonomies, :parent_id
-
-    create_table :decidim_taxonomy_filters do |t|
-      t.integer :taxonomy_id, null: false
-      t.integer :filterable_id, null: false
-      t.string :filterable_type, null: false
-      t.timestamps
-    end
-
-    add_index :decidim_taxonomy_filters, :taxonomy_id
-    add_index :decidim_taxonomy_filters, [:filterable_id, :filterable_type], name: "index_taxonomy_filters_on_fid_and_ftype"
 
     create_table :decidim_taxonomizations do |t|
-      t.integer :taxonomy_id, null: false
-      t.integer :taxonomizable_id, null: false
-      t.string :taxonomizable_type, null: false
+      t.references :taxonomy, null: false, index: true
+      t.references :taxonomizable, null: false, polymorphic: true, index: { name: "index_taxonomizations_on_taxonomizable" }
       t.timestamps
     end
 
-    add_index :decidim_taxonomizations, :taxonomy_id
-    add_index :decidim_taxonomizations, [:taxonomizable_id, :taxonomizable_type], name: "index_taxonomizations_on_tid_and_ttype"
+    add_index :decidim_taxonomizations, [:taxonomy_id, :taxonomizable_id, :taxonomizable_type], name: "index_taxonomizations_on_id_tid_and_ttype", unique: true
   end
 end
