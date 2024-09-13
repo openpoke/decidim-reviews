30bc01f380db1bd22840a4f5de263b22438ed54f
Ivan Vergés <ivan@pokecode.net>
2024-09-13 12:25:50 +0200

    update to shared tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_09_133156) do
+ActiveRecord::Schema[7.0].define(version: 2024_09_13_102247) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
   enable_extension "pg_trgm"
@@ -465,6 +465,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_09_133156) do
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.string "participatory_space_type", null: false
+    t.boolean "visible", default: true
     t.index ["participatory_space_id", "participatory_space_type"], name: "index_decidim_components_on_decidim_participatory_space"
   end
 
@@ -1638,6 +1639,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_09_133156) do
     t.datetime "created_at", precision: nil
     t.datetime "last_used_at", precision: nil
     t.datetime "expires_at", precision: nil
+    t.boolean "registered_only"
     t.index ["decidim_organization_id"], name: "index_decidim_share_tokens_on_decidim_organization_id"
     t.index ["decidim_user_id"], name: "index_decidim_share_tokens_on_decidim_user_id"
     t.index ["token_for_type", "token_for_id"], name: "decidim_share_tokens_token_for"
