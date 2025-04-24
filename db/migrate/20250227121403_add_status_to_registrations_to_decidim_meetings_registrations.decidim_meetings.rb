62e0743dcefc5fd78aaf594ddc8cda4cf44825f7
elviabth <ejbenedith@gmail.com>
2025-04-24 13:27:52 +0200

    update bins

--- db/migrate/20250227121403_add_status_to_registrations_to_decidim_meetings_registrations.decidim_meetings.rb
+++ db/migrate/20250227121403_add_status_to_registrations_to_decidim_meetings_registrations.decidim_meetings.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This migration comes from decidim_meetings (originally 20250408071941)
-# This file has been modified by `decidim upgrade:migrations` task on 2025-04-24 09:24:17 UTC
+# This file has been modified by `decidim upgrade:migrations` task on 2025-04-24 11:27:26 UTC
 class AddStatusToRegistrationsToDecidimMeetingsRegistrations < ActiveRecord::Migration[7.0]
   def change
     add_column :decidim_meetings_registrations, :status, :string, default: "registered"
