0249fe5aa844d5db7ba7d88f97055daeef174991
elviabth <ejbenedith@gmail.com>
2025-05-08 16:30:54 +0200

    fix statistic position and height

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source "https://rubygems.org"
 
 ruby RUBY_VERSION
 
-DECIDIM_VERSION = { github: "decidim/decidim", branch: "feature/fix_history_bug" }
+DECIDIM_VERSION = { github: "decidim/decidim", branch: "feature/fix_statistics_position_and_height" }
 
 gem "decidim", DECIDIM_VERSION
 gem "decidim-conferences", DECIDIM_VERSION
--- db/migrate/20250227121403_add_status_to_registrations_to_decidim_meetings_registrations.decidim_meetings.rb
+++ db/migrate/20250227121403_add_status_to_registrations_to_decidim_meetings_registrations.decidim_meetings.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # This migration comes from decidim_meetings (originally 20250408071941)
-# This file has been modified by `decidim upgrade:migrations` task on 2025-04-25 09:53:27 UTC
+# This file has been modified by `decidim upgrade:migrations` task on 2025-05-08 14:27:30 UTC
 class AddStatusToRegistrationsToDecidimMeetingsRegistrations < ActiveRecord::Migration[7.0]
   def change
     add_column :decidim_meetings_registrations, :status, :string, default: "registered"
