76020f8186fc3fd1a0297a799ae0da1e366be93e
Ivan Vergés <ivan@pokecode.net>
2025-09-18 18:26:13 +0200

    update resutls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_15_130335) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_18_162553) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "ltree"
   enable_extension "pg_trgm"
