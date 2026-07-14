f5cf2e6ef47b17f1417f6315e749084adde70432
David Igón <david@pokecode.net>
2026-07-14 12:07:52 +0200

    apply decidim:upgrade

--- db/migrate/20250212160351_add_reminder_customization_to_decidim_meetings.decidim_meetings.rb
+++ db/migrate/20250212160351_add_reminder_customization_to_decidim_meetings.decidim_meetings.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
-# This migration comes from decidim_meetings (originally 20250206105632)
+# This migration comes from decidim_meetings (originally 20250403094034)
+# This file has been modified by `decidim upgrade:migrations` task on 2026-07-14 10:06:53 UTC
 class AddReminderCustomizationToDecidimMeetings < ActiveRecord::Migration[7.0]
   def change
-    add_column :decidim_meetings_meetings, :reminder_enabled, :boolean, default: false
+    add_column :decidim_meetings_meetings, :reminder_enabled, :boolean, default: true, null: false
     add_column :decidim_meetings_meetings, :send_reminders_before_hours, :integer
     add_column :decidim_meetings_meetings, :reminder_message_custom_content, :jsonb, default: {}, null: false
   end
--- db/migrate/20250408102825_rename_valuation_assignments_count_to_evaluation_assignments_count.decidim_proposals.rb
+++ db/migrate/20250408102825_rename_valuation_assignments_count_to_evaluation_assignments_count.decidim_proposals.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # This migration comes from decidim_proposals (originally 20250121110904)
+# This file has been modified by `decidim upgrade:migrations` task on 2026-07-14 10:06:53 UTC
 class RenameValuationAssignmentsCountToEvaluationAssignmentsCount < ActiveRecord::Migration[7.0]
   def change
     rename_column :decidim_proposals_proposals, :valuation_assignments_count, :evaluation_assignments_count
@@ -9,7 +10,7 @@ class RenameValuationAssignmentsCountToEvaluationAssignmentsCount < ActiveRecord
       dir.up do
         Decidim::Proposals::Proposal.reset_column_information
         Decidim::Proposals::Proposal.unscoped.find_each do |record|
-          Decidim::Proposals::Proposal.reset_counters(record.id, :evaluation_assignments)
+          Decidim::Proposals::Proposal.unscoped.reset_counters(record.id, :evaluation_assignments)
         end
       end
     end
